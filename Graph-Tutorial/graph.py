d86748a79b92109bc400801428ae0b85904d00e0
Jayce Azua <jayce.azua@gmail.com>
2019-08-12 03:23:22 -0700

    [wip] working on tutorial trying to finish...

--- Graph-Tutorial/graph.py
+++ Graph-Tutorial/graph.py
@@ -121,24 +121,135 @@ class Graph:
         for v in g"""
         return iter(self.vert_list.values())
 
-    def make_graph_from_file(filename):
-        # Check if first line is 'G' or 'D' and store the value. If neither, raise an exception
-        # For each vertex id in first line, add a vertex to the graph
-        # For each of the following lines:
-        # Extract the vertex ids and the (optional) weight, and add an edge to the graph
-        # If it is a Graph and not a Digraph, add another edge in the opposite direction
-        # Raise an exception if line contains too many (or too few) items
+    def make_graph_from_file(self, file_name):
+        """Read graph data from a file, and create a graph based on it."""
+        valid_types = "gGdD"
 
-    def get_neighbors(self):
-        # Make sure the input node is actually in the graph
-        # Find all edges for the input node
-        # See what nodes are connected to the input node via the edge
-        # return the connected nodes
+        graph_type = ""
+        vertices = ""
+        edge_list = []
+        directed = False
+        weighted = False
 
-    def breadth_first_search(self, vertex, n):
-        # Make sure the input node is actually in the graph
-        # Run breadth_first_search starting from the input node and going `n` levels deep
-        # Return all nodes found at the `n`th level
+        with open(file_name, 'r') as f:
+            for line in f.readlines():
+                # Strip trailing whitespace
+                line = line.rstrip()
+
+                # Skip line if it is empty to prevent index range errors below
+                if line == "":
+                    # Moves to next line (next iteration of for loop)
+                    continue
+
+                # Find graph type
+                if line[0] in string.ascii_letters:
+                    if line[0] in valid_types:
+                        graph_type = line[0].upper()
+                    else:
+                        raise ValueError("Looking for type 'G' or 'D'")
+
+                # Find list of vertices
+                if line[0] in string.digits:
+                    vertices = line
+
+                # Find edges
+                if line[0] == "(":
+                    edge_list.append(line)
+
+        # See if graph is a digraph
+        if graph_type == "D":
+            directed = True
+        # See if graph is weighted
+        if len(edge_list[0].split(",")) == 3:
+            weighted = True
+
+        # Set the graph type if it has not been set yet
+        if self.num_vertices == 0:
+            self.weighted = weighted
+            self.directed = directed
+
+        # Add vertices to graph
+        # TODO: Does not handle string vertex names
+        for vertex in vertices.split(","):
+            self.add_vertex(int(vertex))
+
+        # Add edges to graph
+        # TODO: Does not handle string vertex names or decimal weights
+        for edge in edge_list:
+            # Remove parenthesis
+            data = edge[1:-1]
+            # Turn data into array by splitting on commas
+            data = data.split(",")
+
+            # Split the tuple correctly
+            if weighted:
+                # Remove parenthesis from strings, and convert strings to ints
+                self.add_edge(int(data[0]), int(data[1]), int(data[2]))
+            else:
+                # Remove parenthesis from strings, and convert strings to ints
+                self.add_edge(int(data[0]), int(data[1]))
+
+
+    def breadth_first_search(self, vertex, n, only_new=True):
+        """Find all vertices n edges away from the passed in vertex."""
+        # Raise error if non vertex object is passed in as vertex
+        if not isinstance(vertex, Vertex):
+            raise TypeError("vertex parameter must be of type Vertex")
+
+        # Raise error if vertex not in the graph
+        if vertex not in self.get_vertices():
+            raise ValueError(f"{vertex} is not in the Graph")
+
+        # If the search is looking for vertices only accessible at level n,
+        if only_new:
+            # Create a set of vertices that have already been visited
+            seen_vertices = set([vertex])
+
+        # Create deque with passed in vertex
+        vertex_deque = deque([vertex])
+        # n_counter tracks the current level
+        n_counter = 0
+        # counter tracks how many vertices from level n are still in the deque
+        counter = 1
+
+        # Keep looping until there are no more vertices to go through, or
+        # until the nth level has been reached
+        while len(vertex_deque) > 0 and n_counter < n:
+            # Grab a vertex from the front of the deque
+            popped_vertex = vertex_deque.popleft()
+
+            # Queue vertices if they will be seen for the first time
+            if only_new:
+                # Go through the neighbors of the popped_vertex
+                for vert in popped_vertex.get_neighbors():
+                    # If this vertex is new, allow it to be traversed
+                    if vert not in seen_vertices:
+                        # Set the parent of this vertex as the popped vertex
+                        vert.parent = popped_vertex
+                        # Add vertex to back of the deque
+                        vertex_deque.append(vert)
+                        # Mark that the vertex has been seen
+                        seen_vertices.add(vert)
+            # Otherwise, just add all vertices
+            else:
+                # Add all vertices that vert can reach to the back of the deque
+                vertex_deque.extend(popped_vertex.get_neighbors())
+            # Remove one from the counter because a vertex was just popped
+            counter -= 1
+
+            # When all nodes from the current level are removed
+            if counter == 0:
+                # Set the current level that all the current vertices are on
+                n_counter += 1
+                # Track how many vertices can be reached on this level
+                counter = len(vertex_deque)
+
+        # If the loop above ends early due to lack of levels,
+        if n_counter < n:
+            # Return empty set because no vertices exist n edges away
+            return set()
+        # Return a set of all the vertices that can be reached at the nth level
+        return set(vertex_deque)
 
     def find_path(self, from_vert, to_vert):
         # Make sure that both nodes from_vert and to_vert are actually in the graph
@@ -148,21 +259,52 @@ class Graph:
         # Since you've been tracking the paths, find the path that goes from from_vert to to_vert
         # Return the path, in the order of nodes visited starting with from_vert and ending with to_vert
         # Driver code
+        pass
 
-    def find_shortest_path(self, A, B):
-        # Make sure that both nodes A and B are actually in the graph
-        # Run BFS starting from A
-        # Figure out a way to keep track of each path you take
-        # Once you find B, end the search.
-        # Since you've been tracking the paths, find the shortest path that goes from A to B
-        # Return the shortest path, in the order of nodes visited starting with A and ending with B
-    
+    def find_shortest_path(self, from_v, to_u):
+        # Raise error if start or end does not exist in graph
+        if from_v not in self.vert_list:
+            raise KeyError(f"Vertex({from_v}) is not in the Graph")
+        if to_u not in self.vert_list:
+            raise KeyError(f"Vertex({to_u}) is not in the Graph")
+
+        # Set the starting and ending vertices, using start and end keys
+        start_vert = self.vert_list[from_v]
+        end_vert = self.vert_list[to_u]
+
+        # Get the vertices one edge away from starting vertex
+        level = 1
+        verts_at_n_level = self.breadth_first_search(start_vert, level)
+        # Keep searching levels there is nothing, or the end vertex is found
+        while end_vert not in verts_at_n_level:
+            # If there are no more vertices to search
+            if len(verts_at_n_level) == 0:
+                # Return None because there is no path between the vertices
+                return None
+            # Get the vertices one more edge away from the starting vertex
+            level += 1
+            verts_at_n_level = self.breadth_first_search(start_vert, level)
+
+        # Create a path list and the ending vertex
+        path = [end_vert]
+        parent = end_vert
+        # Go through the parents of each vertex, until start vertex is reached
+        while start_vert != parent:
+            # Move to the parent of the current vertex, and add it to the path
+            parent = parent.parent
+            path.append(parent)
+
+        # Reverse the path, and return it
+        path[:] = reversed(path)
+        return path
+
     def clique(self):
         # Start with an arbitrary vertex u and add it to the clique
         # For v in remaining vertices not in the clique
         # If v is adjacent to every other vertex already in the clique.
         # Add v to the clique
         # Discard v otherwise
+        pass
 
 
 if __name__ == "__main__":
